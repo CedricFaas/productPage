3d6faee6b3300b50d41635dd88e9a977b25077d1
CedricFaas <s8cefaas@stud.uni-saarland.de>
2022-08-04 13:04:29 +0200

    improved heatmap

--- heatmap.py
+++ heatmap.py
@@ -7,23 +7,23 @@ def heatmap(path,pId):
     file = file[file.ProductId == pId]
     file = file[file.LeftGazePointValidity == 1]
     file = file[file.RightGazePointValidity == 1]
-    file["X"] = round(((file["LeftGazePointX"] + file["RightGazePointX"]) / 2)*100)
-    file["Y"] = round(((file["LeftGazePointY"] + file["RightGazePointY"]) / 2)*100)
-    file = file[file.X <= 100]
-    file = file[file.Y <= 100]
+    file["X"] = round(((file["LeftGazePointX"] + file["RightGazePointX"]) / 2)*1920)
+    file["Y"] = round(((file["LeftGazePointY"] + file["RightGazePointY"]) / 2)*1080)
+    file = file[file.X <= 1920]
+    file = file[file.Y <= 1080]
     file = file[file.X >= 0]
     file = file[file.Y >= 0]
     file = file.drop(file.columns[[0,1,2,3,4,5,6,7,8]], axis=1) 
-    myArray = np.zeros((101,101))
+    myArray = np.zeros((1081,1921))
     grid = pandas.DataFrame(myArray)
     
     file = file.reset_index()
     
     for index, row in file.iterrows():
-        grid.at[int(row["X"]),int(row["Y"])] = grid[int(row["X"])][int(row["Y"])] + 1
-        
-    plt.imshow(grid,cmap='hot',interpolation='nearest', 
-               origin = 'lower')
+        x = int(row["X"])
+        y = int(row["Y"])
+        grid.at[y,x] = grid.at[y,x] + 1.0
+    plt.imshow(grid,cmap='hot',interpolation=None, vmin=0.0, vmax=0.1)
     plt.show()
 
-heatmap('./log/p501/gazeData/gaze_24_3.csv',5)
+heatmap('./log/p501/gazeData/gaze_18_2.csv',1)
